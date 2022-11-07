df8464ddc15ee61d47f942975bdf6c894f093968
Luis Castilleja <luis.castilleja@r2devpros.com>
2022-11-07 14:43:39 -0600

    cambios

---
+++ U2-JuegoFanorona-IA-181G0231-181G0138-181G0533/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace U2_JuegoFanorona_IA_181G0231_181G0138_181G0533
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
